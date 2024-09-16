3c86fbc41fde6f9121c862e1774f35c13543c90b
vishalshrma52 <vishalshrma446@gmail.com>
2024-09-16 23:51:43 +0530

    it is enterprise web application this is created by using the spring boot framework there is two modules inside this application which belongs to a user and a manager in which manager will manage the book and user will purchase it doing transaction

---
+++ src/main/java/com/book/controllers/user/CardController.java
@@ -0,0 +1,46 @@
+package com.book.controllers.user;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.book.models.BookCategory;
+import com.book.models.BookPublisher;
+import com.book.models.CreditCard;
+import com.book.repositories.CategoryRepository;
+import com.book.repositories.CreditCardRepository;
+import com.book.repositories.PublisherRepository;
+import com.book.util.Utility;
+
+@Controller
+@RequestMapping("card")
+public class CardController
+{
+	@Autowired private CreditCardRepository repo;
+	
+	@RequestMapping("details")
+	public String userHome(Model model,HttpSession ses) 
+	{
+		//List<CreditCard> list=repo.findAllByUserid((String)ses.getAttribute("userid"));
+		List<CreditCard> list=repo.findAll();
+		model.addAttribute("list", list);
+		return "user/card/card-details";
+	}
+	@RequestMapping("add")
+	public String getAddCardView() 
+	{
+		return "user/card/add-card";
+	}
+	@RequestMapping("save-card")
+	public String saveCard(CreditCard creditcard) 
+	{
+		repo.save(creditcard);
+		return "user/card/save-card-successfully";
+	}
+}
